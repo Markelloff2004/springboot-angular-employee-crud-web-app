d1777d85c039789659738e61501033c6ce0baa1d
Marcu Starsii <marcustarsii@gmail.com>
2024-11-15 17:30:39 +0200

    Create project with needed dependencies

---
+++ src/main/java/com/example/springbootangularemployeecrudwebapp/SpringbootAngularEmployeeCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootangularemployeecrudwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularEmployeeCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularEmployeeCrudWebAppApplication.class, args);
+	}
+
+}
